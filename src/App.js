46d777f19738ddde92bccc927a3dc563a9328211
Asharma24294 <asharma24294@gmail.com>
2024-10-17 15:52:42 +0530

    over

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Login } from './Components/Login';
 import { Home } from './Components/Home';
 import { CartProvider } from 'react-use-cart';
 import { Cart } from './Components/Cart';
-import { SignUp } from './Components/Signup';
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 function App() {
   return (
@@ -12,13 +11,10 @@ function App() {
 
 <CartProvider>
 <BrowserRouter>
-<Link to="/login"></Link>
-<Link to="/home"></Link>
 <Link to="/"></Link>
+<Link to="/home"></Link>
 <Routes>
-  <Route extact path='/login' element={<Login/>}/>
-  <Route extact path='/' element={<SignUp/>}/>
-
+  <Route extact path='/' element={<Login/>}/>
   <Route extact path='/home' element={<Home/>}/>
   <Route extact path='/cart' element={<Cart/>}/>
 </Routes>
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,73 +1,121 @@
 import React, { useState } from "react";
-import shop from "../image/Amazon_logo.svg (1).png";
+import shop from "../image/Amazon_logo.svg (1).png"
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
-function Login() {
-    const nav = useNavigate();
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
 
-    const updateEmail = (e) => setEmail(e.target.value);
-    const updatePassword = (e) => setPass(e.target.value);
+function Login()
+{
 
-    const check = async () => {
-        if (!email || !pass) {
-            alert("Please enter your email and password");
-            return;
+    let nav=useNavigate();
+
+    let [email,setemail]=useState('');
+
+    const update=(e)=>
+    {
+        setemail(e.target.value)
+    }
+
+    let [pass,setpass]=useState('');
+
+    const update2=(e)=>
+    {
+        setpass(e.target.value)
+    }
+
+let check=()=>
+{
+    if(email=="ankit@gmail.com" && pass==123)
+    {
+    nav("/home")
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
         }
-
-        try {
-            const response = await fetch('http://localhost:5000/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, password: pass }),
-            });
-
-            const data = await response.json();
-            if (response.ok) {
-                nav("/home");
-                Swal.fire({
-                    icon: "success",
-                    title: "Sign in successfully",
-                    toast: true,
-                    position: "top-end",
-                    timer: 3000,
-                    timerProgressBar: true,
-                });
-            } else {
-                alert(data.message);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            alert("An error occurred during login");
-        }
-    };
-
-    return (
-        <div className="login" id="login">
-            <div className="container">
-                <div className="login-in">
-                    <div className="img">
-                        <img src={shop} alt="shop" />
-                    </div>
-                    <div className="box">
-                        <div className="box-in">
-                            <h1>Sign in</h1>
-                            Email or mobile phone number <input type="email" onChange={updateEmail} />
-                            Password <input type="password" onChange={updatePassword} />
-                            <button onClick={check}>Sign in</button>
-                            <div className="create">
-                                <button onClick={() => nav("/")}>Create an account</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+      });
+      Toast.fire({
+        icon: "success",
+        title: "Sign in successfully"
+      });
+    }
+    else if(email==0 && pass==0){
+        alert("Enter your E-mail & Password")
+    }
+    else if(email==0 && pass==123)
+    {
+        alert("Enter your email")
+    }
+    else if(email=="ankit@gmail.com" && pass==0)
+    {
+        alert("Enter your Password")
+    }
+    else if(email!="ankit@gmail.com" && pass==123)
+    {
+        alert("Check your E-mail")
+    }
+    else if(email=="ankit@gmail.com" && pass!=123)
+    {
+        alert("Check your Password")
+    }
+    else
+    {
+        alert("Enter correct details")
+    }
 }
 
-export { Login };
+
+
+
+
+    return(
+        <>
+
+<div className="login" id="login">
+    <div className="container">
+        <div className="login-in">
+       <div className="img">
+        <img src={shop} alt="shop"/>
+
+       </div>
+       <div className="box">
+        <div className="box-in">
+
+        
+        <h1>Sign in</h1>
+
+        
+        Email or mobile phone number<input type="email" onChange={update} />
+        Password <input type="password" onChange={update2} />
+
+
+        <a href="https://www.google.com/search?q=amazon+log+in+page&rlz=1C1RXQR_enIN1011IN1011&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjD6tSB6pn_AhVGVPUHHdFXDo0Q_AUoAnoECAEQBA&biw=1536&bih=714&dpr=1.25#imgrc=2_GIBEHf5Uj2YM">Forgot password ?</a>
+        
+        <button onClick={check}>Sign in</button>
+
+
+        {/* <div className="new"> */}
+      {/* <p>------------- New to Amazon ?------------</p> */}
+        {/* </div> */}
+
+        <div className="create">
+<button>Create an account</button>
+        </div>
+
+        </div>
+       </div>
+        </div>
+
+    </div>
+
+</div>
+        
+        </>
+    )
+}
+export{Login}
